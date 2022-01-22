b4e709b674421e6ca118a0d01fadde708e18728c
iinsue <hinsu93@naver.com>
2022-01-22 18:59:48 +0900

    The Art of React 14.5
    데이터 연동하기 372p

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import NewsList from "./components/NewsList";
 
-import { useState } from "react";
-import axios from "axios";
-
 function App() {
-  const [data, setData] = useState(null);
-  const SECRET_KEY = process.env.REACT_APP_NEWS_API_KEY;
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${SECRET_KEY}`
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
   return <NewsList />;
 }
 
